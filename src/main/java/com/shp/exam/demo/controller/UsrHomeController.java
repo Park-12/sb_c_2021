439ca52c64c2b2575b514059f39577bf906c7f3a
Park-12 <sohyunp96@gmail.com>
2021-09-07 11:26:45 +0900

    count 조절 액션

--- src/main/java/com/shp/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/shp/exam/demo/controller/UsrHomeController.java
@@ -13,41 +13,17 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		count = -1;
 	}
-	
-	@RequestMapping("/usr/home/main")
+		
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}
-	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		/* 
-		 * 지역변수의 값은 유지 않됨
-		 * int count = 0;
-		 * '1'만 나옴
-		*/
-		count++;
+	public int getCount() {
 		return count;
 	}
 	
-	@RequestMapping("/usr/home/main5")
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count의 값이" + this.count + "(으)로 초기화 되었습니다.";
 	}
 }
