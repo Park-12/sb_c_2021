dee54f4dc916c839a20d934b035db637e2069558
Park-12 <sohyunp96@gmail.com>
2021-09-07 10:37:15 +0900

    count변수의 값 초기화 액션 구현

--- src/main/java/com/shp/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/shp/exam/demo/controller/UsrHomeController.java
@@ -6,10 +6,48 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	// 전역변수로 하면 객체 죽기 전까지 살아있음
+	private int count;
+	
+	// 초기화
+	public UsrHomeController() {
+		count = -1;
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요.";
 	}
 	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요.";
+	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		/* 
+		 * 지역변수의 값은 유지 않됨
+		 * int count = 0;
+		 * '1'만 나옴
+		*/
+		count++;
+		return count;
+	}
+	
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 되었습니다.";
+	}
 }
