ad7d62d146a5291677e21c9a248739180d019fa6
Park-12 <sohyunp96@gmail.com>
2021-09-12 21:11:25 +0900

    로그인 체크, 작성자 정보 저장

--- src/main/java/com/shp/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/shp/exam/demo/controller/UsrArticleController.java
@@ -2,6 +2,8 @@ package com.shp.exam.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,19 @@ public class UsrArticleController {
 	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData<Article> doAdd(String title, String body) {
+	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+		
 		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
@@ -31,7 +45,7 @@ public class UsrArticleController {
 		
 		// int id = (int) writeArticleRd.getData1();
 		// ResultData<Integer> 하면 형변환 X
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 		int id = writeArticleRd.getData1();
 		
 		Article article = articleService.getArticle(id);
--- src/main/java/com/shp/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/shp/exam/demo/repository/ArticleRepository.java
@@ -10,7 +10,7 @@ import com.shp.exam.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 	// INSERT는 입력값이기 때문에 리턴값이 없다.
-	public void writeArticle(@Param("title") String title, @Param("body") String body);
+	public void writeArticle(@Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
 	
 	public Article getArticle(@Param("id") int id);
 
