ae902758307bcb7d9557ce94c62f8901a0b581e9
Park-12 <sohyunp96@gmail.com>
2021-09-12 19:08:59 +0900

    ResultData

--- src/main/java/com/shp/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/shp/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shp.exam.demo.service.ArticleService;
+import com.shp.exam.demo.util.Ut;
 import com.shp.exam.demo.vo.Article;
+import com.shp.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -62,14 +64,14 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 	// 액션 메서드 끝
 }
