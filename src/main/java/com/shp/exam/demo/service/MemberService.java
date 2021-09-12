cc96e99fdd5aa780af38eb1a2361c8e96deba1c7
Park-12 <sohyunp96@gmail.com>
2021-09-12 19:49:48 +0900

    doJoin -> ResultData

--- src/main/java/com/shp/exam/demo/service/MemberService.java
+++ src/main/java/com/shp/exam/demo/service/MemberService.java
@@ -3,7 +3,9 @@ package com.shp.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.shp.exam.demo.repository.MemberRepository;
+import com.shp.exam.demo.util.Ut;
 import com.shp.exam.demo.vo.Member;
+import com.shp.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
@@ -15,25 +17,27 @@ public class MemberService {
 	}
 
 	// int = 가입된 회원의 번호임
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		// 로그인 아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
 		
 		// oldMember가 있을 때
 		// 숫자 리턴
 		if (oldMember != null) { 
-			return -1;
+			return ResultData.from("F-7", Ut.f("'%s'는 이미 사용 중인 로그인 아이디입니다.", loginId));
 		}
 		
 		// 이름+이메일 중복체크
 		oldMember = getMemberByNameAndEmail(name, email);
 		
 		if (oldMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("'%s'와(과) '%s'(은)는 이미 사용 중입니다.", name, email));
 		}
 		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		
+		return ResultData.from("S-1","회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
